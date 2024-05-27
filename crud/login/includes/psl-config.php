597dccea1e86781116c20893a119af484caeda5c
joao <joao.gabrielferreira1@outlook.com>
2024-05-27 17:27:30 -0300

    Aula 27/05 (não terminado).

---
+++ crud/login/includes/psl-config.php
@@ -0,0 +1,9 @@
+<?php
+    define("HOST","localhost"); //Define HOST como localhost.
+    define("USER","sec_user"); //Define USER como sec_user.
+    define("PASSWORD","4Fa98xkHVd2XmnfK"); //Define PASSWORD como 4Fa98xkHVd2XmnfK.
+    define("DATABASE","secure_login"); //Define DATABASE como secure_login.
+    define("CAN_REGISTER","any"); //Define CAN_REGISTER como any.
+    define("DEFAULT_ROLE","member"); //Define DEFAULT_ROLE como member.
+    define("SECURE","FALSE"); //Define SECURE como FALSE.
+?>
